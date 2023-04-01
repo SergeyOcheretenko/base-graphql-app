e9bd28c2c77bc7d533c391bde28b5a418f4fb972
SergeyOcheretenko <ocheretenko.s@gmail.com>
2023-04-01 10:46:35 +0300

    feat: add Hobby type and resolver

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,21 +10,60 @@ import {
 
 // Dummy data
 const users = [
-  { id: '1', name: 'Bond', age: 36 },
-  { id: '2', name: 'Anna', age: 26 },
-  { id: '3', name: 'Bella', age: 18 },
-  { id: '4', name: 'Gina', age: 20 },
-  { id: '5', name: 'Georgina', age: 36 },
+  { id: '1', name: 'Bond', age: 36, profession: 'Programmer' },
+  { id: '2', name: 'Anna', age: 26, profession: 'Baker' },
+  { id: '3', name: 'Bella', age: 18, profession: 'Mechanic' },
+  { id: '4', name: 'Gina', age: 20, profession: 'Painter' },
+  { id: '5', name: 'Georgina', age: 36, profession: 'Teacher' },
+];
+
+const hobbies = [
+  {
+    id: '1',
+    title: 'Programming',
+    description: 'Using computers to make the world a better place',
+  },
+  {
+    id: '2',
+    title: 'Rowing',
+    description: 'Sweat and feel better before eating donuts',
+  },
+  {
+    id: '3',
+    title: 'Swimming',
+    description: 'Get in the water and learn to become the water',
+  },
+  {
+    id: '4',
+    title: 'Fencing',
+    description: 'A hobby for fency people',
+  },
+  {
+    id: '5',
+    title: 'Hiking',
+    description: 'Wear hiking boots and explore the world',
+  },
 ];
 
 // Types
 const UserType = new GraphQLObjectType({
   name: 'User',
-  description: 'Documentation for user',
+  description: 'User description',
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
+    profession: { type: GraphQLString },
+  }),
+});
+
+const HobbyType = new GraphQLObjectType({
+  name: 'Hobby',
+  description: 'Hobby description',
+  fields: () => ({
+    id: { type: GraphQLID },
+    title: { type: GraphQLString },
+    description: { type: GraphQLString },
   }),
 });
 
@@ -38,6 +77,13 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return users.find((user) => user.id === args.id);
+      },
+    },
+    hobby: {
+      type: HobbyType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return hobbies.find((hobby) => hobby.id === args.id);
       },
     },
   }),
