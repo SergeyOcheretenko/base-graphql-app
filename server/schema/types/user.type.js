5186b65b3bc58cb0632a79fce6e54c09339fb1ff
SergeyOcheretenko <ocheretenko.s@gmail.com>
2023-04-01 13:30:59 +0300

    feat: show posts and hobbies for users

--- server/schema/types/user.type.js
+++ server/schema/types/user.type.js
@@ -5,7 +5,11 @@ import {
   GraphQLID,
   GraphQLString,
   GraphQLInt,
+  GraphQLList,
 } from 'graphql';
+import { hobbies, posts } from '../dummies.js';
+import { HobbyType } from './hobby.type.js';
+import { PostType } from './post.type.js';
 
 export const UserType = new GraphQLObjectType({
   name: 'User',
@@ -15,5 +19,17 @@ export const UserType = new GraphQLObjectType({
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
     profession: { type: GraphQLString },
+    posts: {
+      type: new GraphQLList(PostType),
+      resolve(parent, args) {
+        return posts.filter((post) => post.userId === parent.id);
+      },
+    },
+    hobbies: {
+      type: new GraphQLList(HobbyType),
+      resolve(parent, args) {
+        return hobbies.filter((hobby) => hobby.userId === parent.id);
+      },
+    },
   }),
 });
